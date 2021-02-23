eeb66f277e7a32f312ae6b48473fbdc4eec8507c
Pietro <pietro.giacomazzi@gmail.com>
2021-02-23 21:07:00 +0100

    spaghettone

---
+++ bots/greedyghost.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+
+from discord.ext import commands
+import random, sys, configparser
+import support.vtm_res
+
+if len(sys.argv) == 1:
+    print("Specifica un file di configurazione!")
+
+config = configparser.ConfigParser()
+config.read(sys.argv[1])
+
+TOKEN = config['Discord']['token']
+
+
+SOMMA_CMD = ["somma", "s"]
+DIFF_CMD = ["diff", "d"]
+MULTI_CMD = ["multi", "m"]
+DANNI_CMD = ["danni", "dmg"]
+PROGRESSI_CMD = ["progressi", "p"]
+SPLIT_CMD = ["split"]
+
+NORMALE = 0
+SOMMA = 1
+DANNI = 2
+PROGRESSI = 3
+
+bot = commands.Bot(['gg', '.'])
+
+#executed once on bot boot
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        print(
+            f'{bot.user} is connected to the following guild:\n'
+            f'{guild.name} (id: {guild.id})'
+        )
+    #members = '\n - '.join([member.name for member in guild.members])
+    #print(f'Guild Members:\n - {members}')
+    #await bot.get_channel(int(config['DISCORD_DEBUG_CHANNEL'])).send("bot is online")
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    print(f"On_error: {args[0]}")
+    #await bot.get_channel(int(config['DISCORD_DEBUG_CHANNEL'])).send(f'Unhandled message: {args[0]}')
+    raise
+
+@bot.command(name='coin', help = 'Tira una moneta')
+async def coin(ctx):
+    response = "Testa" if random.randint(0, 1) else "Croce"
+    await ctx.send(response)
+
+
+@bot.command(name='roll', help = 'Soon™')
+async def roll(ctx, *args):
+    #print("roll args:", repr(args))
+    try:
+        if len(args) == 0:
+            raise ValueError("roll cosa diomadonna")
+        split = args[0].split("d")
+        if len(split) > 2:
+            raise ValueError("Troppe 'd' b0ss")
+        if len(split) == 1:
+            raise ValueError(f'"{split[0]}" cosa')
+        if not split[0].isdigit():
+            raise ValueError(f'"{split[0]}" non è un numero intero')
+        if not split[1].isdigit():
+            raise ValueError(f'"{split[1]}" non è un numero intero')
+        n = int(split[0])
+        faces = int(split[1])
+        if n <= 0:
+            raise ValueError(f'{n} non è un numero <= 0')
+        if faces <= 0:
+            raise ValueError(f'{faces} non è un numero <= 0')
+        if n > max_dice:
+            raise ValueError(f'{n} dadi sono troppi b0ss')
+        if faces > max_faces:
+            raise ValueError(f'{faces} facce sono un po\' tante')
+        if len(args) == 1: #simple roll
+            raw_roll = list(map(lambda x: random.randint(1, faces), range(n)))
+            response = repr(raw_roll)
+        else:
+            diff = None
+            multi = None
+            split = None
+            rolltype = 0 # somma, progressi...
+            add = None # extra successi
+            i = 1
+            while i < len(args):
+                if args[i] in SOMMA_CMD:
+                    rolltype = SOMMA
+                elif args[i] in DIFF_CMD:
+                    if diff:
+                        raise ValueError(f'eeh deciditi')
+                    if len(args) == i+1:
+                        raise ValueError(f'diff cosa')
+                    if not args[i+1].isdigit():
+                        raise ValueError(f'"{args[i+1]}" non è una difficoltà valida')
+                    diff = int(args[i+1])
+                    if diff > 10 or diff < 2:
+                        raise ValueError(f'{args[i+1]} non è una difficoltà valida')
+                    i += 1 
+                elif args[i] in MULTI_CMD:
+                    if multi:
+                        raise ValueError(f'eeh deciditi')
+                    if len(args) == i+1:
+                        raise ValueError(f'multi cosa')
+                    if not args[i+1].isdigit():
+                        raise ValueError(f'"{args[i+1]}" non è un numero di mosse valido')
+                    multi = int(args[i+1])
+                    if multi < 2:
+                        raise ValueError(f'una multipla deve avere almeno 2 tiri!')
+                    if n-multi-(multi-1) <= 0:
+                        raise ValueError(f'non hai abbastanza dadi per questo numero di mosse!')
+                    i += 1
+                elif args[i] in DANNI_CMD:
+                    rolltype = DANNI
+                elif args[i] in PROGRESSI_CMD:
+                    rolltype = PROGRESSI
+                elif args[i] in SPLIT_CMD:
+                    raise ValueError(f'Non implementato')
+                elif args[i].startswith("+"):
+                    raise ValueError(f'Non implementato')
+                else:
+                    raise ValueError(f'coes')
+                i += 1
+            if multi:
+                if rolltype == NORMALE:
+                    response = ""
+                    if split:
+                        pass 
+                    else:
+                        if not diff:
+                            raise ValueError(f'Si ma mi devi dare una difficoltà')
+                    for i in range(multi):
+                        raw_roll = list(map(lambda x: random.randint(1, faces), range(n-i-multi)))
+                        # todo split index
+                        successi, tiro = vtm_res.decider(sorted(raw_roll), diff)
+                        response += f'Azione {i+1}: {successi} successi a diff {diff}, tiro: {raw_roll}\n'
+                else:
+                    raise ValueError(f'Combinazione di parametri non supportata')
+            else: # 1 tiro solo 
+                raw_roll = list(map(lambda x: random.randint(1, faces), range(n)))
+                if split:
+                    pass
+                else:
+                    if rolltype == NORMALE: # tiro normale
+                        if not diff:
+                            raise ValueError(f'Si ma mi devi dare una difficoltà')
+                        successi, tiro = vtm_res.decider(sorted(raw_roll), diff)
+                        response = f'{successi} successi a diff {diff}, tiro: {raw_roll}'
+                    elif rolltype == SOMMA:
+                        somma = sum(raw_roll)
+                        response = f'somma: {somma}, tiro: {raw_roll}'
+                    elif rolltype == DANNI:
+                        if not diff:
+                            diff = 6
+                        successi, tiro = vtm_res.decider(sorted(raw_roll), diff,  failcancel = 0)
+                        response = f'{successi} danni, tiro: {raw_roll}'
+                        if diff != 6:
+                            response += f' (diff {diff})'
+                    elif rolltype == PROGRESSI:
+                        if not diff:
+                            diff = 6
+                        successi, tiro = vtm_res.decider(sorted(raw_roll), diff, failcancel = 0, spec = True, spec_reroll = False)
+                        response = f'progressi: {successi}, tiro: {raw_roll}'
+                        if diff != 6:
+                            response += f' (diff {diff})'
+                    else:
+                        raise ValueError(f'Tipo di tiro sconosciuto: {rolltype}')
+            
+    except ValueError as e:
+        response = e
+    await ctx.send(response)
+
+
+
+bot.run(TOKEN)
---
+++ bots/support/vtm_res.py
@@ -0,0 +1,131 @@
+import random
+
+"""
+# soak roll: 1s cancel, but no critfail (pg 306)
+# dmg  roll: 1s cancel, but no critfail. Commonly houseruled with no canceling
+# atk  roll: 1s cancel and can critfail
+
+# crit failing: no successes and 1s present in the roll. if there are more 1s than successes, it's still a normal fail! (v20)
+"""
+
+# failcancel: 0: no canceling, 1: canceling, but no critfails, 2: full canceling and critfailing, 3: revised critfailing
+# spec: specialization effect for 10s
+# cancel_high: cancel successes from lowest (false), or highest (True). only matters when specializations apply. DEFAULT ?
+# spec_reroll: true: reroll dei 10, false: doppio successo DEFAULT FALSE
+
+def decider(roll_raw, difficulty, failcancel = 2, spec = False, cancel_high = True, spec_reroll = False):
+    md = -1 # will contain the index of the first success
+    for i in range(0, len(roll_raw)):
+        if roll_raw[i] >= difficulty:
+            md = i
+            break
+    if md == -1:
+        md = len(roll_raw)
+    successes = roll_raw[md:] # filter successes out of the roll
+    count_success = len(successes)
+    crit_fails = roll_raw.count(1)
+    if failcancel > 0: # and crit_fails > 0
+        if failcancel == 2 and  crit_fails > 0 and count_success == 0: # critfail
+            return -1, roll_raw
+        elif failcancel == 3 and  crit_fails > count_success: # revised critfail
+            return -1, roll_raw
+        else: #just cancel
+            if cancel_high and crit_fails > 0:
+                successes = successes[:-crit_fails]
+            else:
+                successes = successes[crit_fails:]
+            count_success = len(successes)
+    tens = successes.count(10)
+    if spec:
+        if spec_reroll:
+            additional = 0
+            for i in range(0, tens):
+                if random.randint(1, 10)>= difficulty:
+                    additional += 1
+            return count_success+additional, roll_raw
+        else:
+            return count_success+tens, roll_raw
+    else:
+        return count_success, roll_raw
+
+
+def rollpool(dicepool, difficulty, failcancel = 2, spec = False, cancel_high = False, spec_reroll = False):
+    if dicepool == 0:
+        print("0 dice")
+    roll_raw = sorted(list(map(lambda x: random.randint(1, 10), range(0, dicepool))))
+    return decider(roll_raw, difficulty, failcancel, spec, cancel_high, spec_reroll)
+
+
+if __name__ == "__main__":
+    for i in range(0, 100):
+        print(rollpool(10, 8, 2, False, True, False))
+    test_rolls = [[1],
+                  [3, 6, 6, 8],
+                  [6],
+                  [5, 7, 8],
+                  [7],
+                  [5, 7],
+                  [8, 9],
+                  [5],
+                  [2, 4, 4, 5, 6, 6, 6, 6, 8, 9],
+                  [5, 5, 6, 10],
+                  [1, 4, 8, 8, 9],
+                  [3],
+                  [4, 4, 8],
+                  [1, 1, 3, 3, 3, 4, 6, 7, 8, 9],
+                  [2, 3, 6],
+                  [3, 4, 5, 6, 10],
+                  [7, 8, 10],
+                  [1, 2, 7, 7, 9, 10, 10],
+                  [3, 5, 8, 8, 10],
+                  [1, 3, 9],
+                  [1, 1, 4, 4, 5, 6, 9, 9, 10],
+                  [1, 6, 9, 9, 10],
+                  [1, 1, 3, 3, 4, 6, 8, 9, 9],
+                  [1, 1, 1, 1, 2, 6, 6],
+                  [1, 3, 7, 8],
+                  [8],
+                  [1, 3, 8],
+                  [3, 8, 10],
+                  [5, 7, 9],
+                  [2, 3, 3, 5, 5, 6, 7, 10],
+                  [1, 2, 4, 6, 6, 8, 8, 9],
+                  [2, 5, 5, 8, 9, 10],
+                  [2, 4, 6, 6, 7, 8],
+                  [5, 8, 8, 8, 9, 10, 10, 10],
+                  [3, 3, 5, 6, 8, 8, 9],
+                  [1, 2, 3, 7, 7, 7, 7, 7, 8, 10],
+                  [1, 1, 2, 5, 9, 10],
+                  [1, 1, 4, 5, 6, 10],
+                  [3, 8],
+                  [7, 9],
+                  [1, 10],
+                  [1, 2, 4, 4, 4, 6, 9, 10],
+                  [1, 2, 3, 4, 7, 10],
+                  [1, 1, 2, 2, 2, 2, 4, 9, 9],
+                  [5, 10],
+                  [1, 1, 5, 6, 9, 9, 9, 10],
+                  [7, 8],
+                  [1, 4, 5, 6, 7, 8, 10],
+                  [1, 5, 7, 9, 10]]
+    # fc 0, nospec, highcancel, anyreroll
+    assert decider([1, 1, 1, 1, 2, 6, 6], 6, 0, False, True, False)[0] == 2
+    assert decider([1, 1, 1, 1, 2, 6, 6], 6, 0, False, False, False)[0] == 2
+    assert decider([1, 1, 1, 1, 2, 6, 6], 6, 0, False, False, True)[0] == 2
+    assert decider([1, 1, 1, 1, 2, 6, 6], 6, 0, False, True, True)[0] == 2
+    for dice in range(0, 10):
+        for tens in range(0, dice):
+            for ones in range(0, dice-tens):
+                for successes in range(0, dice-tens-ones):
+                    for difficulty in range(3, 10):
+                        roll = sorted([10]*tens+[1]*ones+[random.randint(difficulty, 9) for i in range(0, successes)]+[random.randint(2, difficulty-1) for i in range(0, dice-ones-tens-successes)])
+                        assert decider(roll, difficulty, 3, False, True, False)[0] == max(-1, tens+successes-ones)
+                        # 2
+                        if successes+tens > 0: # botch not possible
+                            assert decider(roll, difficulty, 2, False, True, False)[0] >= 0
+                            if successes+tens <= ones:
+                                assert decider(roll, difficulty, 2, False, True, False)[0] == 0
+                            else:
+                                assert decider(roll, difficulty, 2, False, True, False)[0] == tens+successes-ones
+                        # 1 cancel no botch
+                        
