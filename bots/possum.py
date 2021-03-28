4e389616ad0e9df8fd9c1f02b676675ff62f63d5
Pietro <pietro.giacomazzi@gmail.com>
2021-03-28 18:49:15 +0200

    cambio stato randomico

--- bots/possum.py
+++ bots/possum.py
@@ -15,8 +15,17 @@ config.read(sys.argv[1])
 TOKEN = config['Discord']['token']
 
 
-
 bot = commands.Bot(['gg', '.'], help_command=None)
+
+
+attivita = [
+    (discord.ActivityType.watching, "il polpaccio di Rossellini"),
+    (discord.ActivityType.watching, "Jerome con odio"),
+    (discord.ActivityType.playing,  "all'impiccato con Joshua"),
+    (discord.ActivityType.watching, "l'immondizia"),
+    (discord.ActivityType.watching, "l'immondizia. Cioè Rossellini"),
+    (discord.ActivityType.watching, "Rossellini con aria affamata")
+    ]
 
 #executed once on bot boot
 @bot.event
@@ -26,7 +35,9 @@ async def on_ready():
             f'{bot.user} is connected to the following guild:\n'
             f'{guild.name} (id: {guild.id})'
         )
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Rossellini con aria affamata"))
+    acttype, actstring = random.choice(attivita)
+    await bot.change_presence(activity=discord.Activity(type=acttype, name=actstring))   
+    #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Rossellini con aria affamata"))
     #members = '\n - '.join([member.name for member in guild.members])
     #print(f'Guild Members:\n - {members}')
     #await bot.get_channel(int(config['DISCORD_DEBUG_CHANNEL'])).send("bot is online")
@@ -36,8 +47,6 @@ async def on_error(event, *args, **kwargs):
     print(f"On_error: {args[0]}")
     #await bot.get_channel(int(config['DISCORD_DEBUG_CHANNEL'])).send(f'Unhandled message: {args[0]}')
     raise
-
-
 
 @bot.command('help')
 async def help(context, *args):
@@ -50,59 +59,62 @@ async def help(context, *args):
                 ]
             await context.send(f"Ti dirò quello che penso di {random.choice(epiteti)} di un Rossellini")
 
+
 @bot.command('rossellini')
 async def rossellini(ctx):
-	insulti=['Ti stacco i tendini a morsi, sciacquatore di palle' ,
- 		'Pezzo di Fango che mi incrosta il pelo' ,
-		'Speleologo dei culi rotti' ,
-		'Ossa, mafia e mandolino' ,
-		'AAAAAAAAAAAAAAAAAAAAAAAAA' ,
-		'Sei peggio della spazzatura che mastico ogni giorno' ,
-		'Non puoi morire, finché non ti ammazzo io' ,
-		'Kkkekkkkekkkkkekkke' ,
-		'Meglio un cassonetto oggi che un Rossellini tutta la vita' ,
-		'Soporifero come le esalazioni fognarie' ,
-		'La tua famiglia è come un tesoro: ti servono mappa e pala per trovarla' ,
-		'Ti odio più di quella cagna di tua cugina' ,
-		'Sacchetto de monnezza' ,
-		'Le rose sono rosse, le viole son blu, il primo a crepare sarai proprio tu' ,
-		"Hai un bidone dell'immondizia al posto del cuore, e io sono affamato" ,
-		'Rossellini fa rima con "ti scortico la faccia"' ,
-		'Evocami di nuovo e ti cavo gli occhi' ,
-		'+1 ad Iniziativa' ,
-		'è stato subito odio a prima vista'  ,
-		'Io mi fingo morto, tu lo sarai davvero' ,
-		'è meglio se inizi a correre' ,
-		'Quando sarà buio ti troverò e ti riempirò di botte' ,
-		'Kkkrrkkkekkkkekkkkkrkekkkkekkekkkkkkkrekkkkkekkekkkekkrkekk' ,
-		'Ho mangiato mele marce molto più dignitose di te' ,
-		'Ho trovato una cosa interessante nella pattumiera: la tua faccia' ,
-		'Il ratto che vive nella pattumiera accanto ha più palle di te' ,
-		'Riuscirai a fare schifo anche oggi? Si.. ovvio che si' ,
-		'Ma tu guarda! Devo insultarti ancora, che lavoro delizioso. Fai schifo.' ,
-		'Sarebbe stato più produttivo rovistare nella casella postale della discarica' ,
-		'Non ti bastava essere Italiano? Dovevi per forza essere anche osceno?' ,
-		"Nessuno si stanca mai di ricordarti che è meglio il bidone dell'umido" ,
-		'Oggi nel bidone della monnezza ho trovato le tue palle, da quanto le avevi perse?' ,
-		'Sei più imbarazzante del tuo amico nero' ,
-		'Prima o poi riuscirò a morderti davvero' ,
-		'Guardarti in faccia rischia di farmi morire di nuovo' ,
-		'Maledetto Rossellini' ,
-		'Oggi mi sento buono, +1 iniziativa ma non a Rossellini' ,
-		'+1 ad iniziativa solo a Rossellini, così crepa prima' ,
-		'Resuscita la vastità del cazzo che me ne frega di te' ,
-		"Lorenzo sai che ora è? E' ora che ti levi dal cazzo" ,
-		'Entra nel tuo ambiente naturale: muori' ,
-		'Ho trovato una cosa che ti farebbe bene: un sacco pieno di ceffoni' ,
-		'Hai il carisma di una fetta merdosa di una torta al guano' ,
-		'Con quel muso mi fai perdere il pelo fantasma che mi rimane' ,
-		"+ 1 all'iniziativa di chi ti insulta nei prossimi 3 secondi" ,
-		'Maledetta quella volta che mamma Rossellini e papà Rossellini hanno deciso di vomitare assieme' ,
-		'Non potevi rimanere in Italia? Qui abbiamo già i nostri problemi del cazzo' ,
-		'La prossima volta chiedi aiuto al Papa' ,
-			 
-		 ]
-	await ctx.send(f'{random.choice(insulti)}')
+    if random.random() < float(config['Settings']['activity_pct']):
+        acttype, actstring = random.choice(attivita)
+        await bot.change_presence(activity=discord.Activity(type=acttype, name=actstring))
+    insulti=['Ti stacco i tendini a morsi, sciacquatore di palle' ,
+            'Pezzo di Fango che mi incrosta il pelo' ,
+            'Speleologo dei culi rotti' ,
+            'Ossa, mafia e mandolino' ,
+            'AAAAAAAAAAAAAAAAAAAAAAAAA' ,
+            'Sei peggio della spazzatura che mastico ogni giorno' ,
+            'Non puoi morire, finché non ti ammazzo io' ,
+            'Kkkekkkkekkkkkekkke' ,
+            'Meglio un cassonetto oggi che un Rossellini tutta la vita' ,
+            'Soporifero come le esalazioni fognarie' ,
+            'La tua famiglia è come un tesoro: ti servono mappa e pala per trovarla' ,
+            'Ti odio più di quella cagna di tua cugina' ,
+            'Sacchetto de monnezza' ,
+            'Le rose sono rosse, le viole son blu, il primo a crepare sarai proprio tu' ,
+            "Hai un bidone dell'immondizia al posto del cuore, e io sono affamato" ,
+            'Rossellini fa rima con "ti scortico la faccia"' ,
+            'Evocami di nuovo e ti cavo gli occhi' ,
+            '+1 ad Iniziativa' ,
+            'è stato subito odio a prima vista'  ,
+            'Io mi fingo morto, tu lo sarai davvero' ,
+            'è meglio se inizi a correre' ,
+            'Quando sarà buio ti troverò e ti riempirò di botte' ,
+            'Kkkrrkkkekkkkekkkkkrkekkkkekkekkkkkkkrekkkkkekkekkkekkrkekk' ,
+            'Ho mangiato mele marce molto più dignitose di te' ,
+            'Ho trovato una cosa interessante nella pattumiera: la tua faccia' ,
+            'Il ratto che vive nella pattumiera accanto ha più palle di te' ,
+            'Riuscirai a fare schifo anche oggi? Si.. ovvio che si' ,
+            'Ma tu guarda! Devo insultarti ancora, che lavoro delizioso. Fai schifo.' ,
+            'Sarebbe stato più produttivo rovistare nella casella postale della discarica' ,
+            'Non ti bastava essere Italiano? Dovevi per forza essere anche osceno?' ,
+            "Nessuno si stanca mai di ricordarti che è meglio il bidone dell'umido" ,
+            'Oggi nel bidone della monnezza ho trovato le tue palle, da quanto le avevi perse?' ,
+            'Sei più imbarazzante del tuo amico nero' ,
+            'Prima o poi riuscirò a morderti davvero' ,
+            'Guardarti in faccia rischia di farmi morire di nuovo' ,
+            'Maledetto Rossellini' ,
+            'Oggi mi sento buono, +1 iniziativa ma non a Rossellini' ,
+            '+1 ad iniziativa solo a Rossellini, così crepa prima' ,
+            'Resuscita la vastità del cazzo che me ne frega di te' ,
+            "Lorenzo sai che ora è? E' ora che ti levi dal cazzo" ,
+            'Entra nel tuo ambiente naturale: muori' ,
+            'Ho trovato una cosa che ti farebbe bene: un sacco pieno di ceffoni' ,
+            'Hai il carisma di una fetta merdosa di una torta al guano' ,
+            'Con quel muso mi fai perdere il pelo fantasma che mi rimane' ,
+            "+ 1 all'iniziativa di chi ti insulta nei prossimi 3 secondi" ,
+            'Maledetta quella volta che mamma Rossellini e papà Rossellini hanno deciso di vomitare assieme' ,
+            'Non potevi rimanere in Italia? Qui abbiamo già i nostri problemi del cazzo' ,
+            'La prossima volta chiedi aiuto al Papa' ,
+             ]
+    await ctx.send(f'{random.choice(insulti)}')
 
 
 @bot.command(aliases=['paradiso' , 'torta'] , brief='Ricorda a Sam il vero Vietnam' , help = 'Il regno del colesterolo')
