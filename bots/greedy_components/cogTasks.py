44113daa129170377e777ccd447eb0f7f0c169cb
Pietro Giacomazzi <pietro.giacomazzi@gmail.com>
2022-02-12 19:37:25 +0100

    compacted messages

--- bots/greedy_components/cogTasks.py
+++ bots/greedy_components/cogTasks.py
@@ -17,31 +17,32 @@ class GreedyGhostCog_Tasks(commands.Cog):
     def cog_unload(self):
         self.userMaintenance.cancel()
 
-    @tasks.loop(seconds=3600)
+    @tasks.loop(seconds=3600*24)
     async def userMaintenance(self):
         usersSeenDict = {}
         for usr in self.bot.dbm.getUsers():
             usersSeenDict[int(usr['userid'])] = False
+
+        added = []
+        removed = []
         
         for guild in self.bot.guilds:
             for member in guild.members:
                 if member.id in usersSeenDict:
                     if not usersSeenDict[member.id]:
                         self.bot.dbm.updateUser(member.id, member.name)
-                        await self.bot.logToDebugUser(f"user maintenance: updated {member.name}")
                 else:
                     self.bot.dbm.registerUser(member.id, member.name, self.bot.config['BotOptions']['default_language'])
-                    await self.bot.logToDebugUser(f"user maintenance: registered {member.name}")
+                    added.append(member.name)
                 
                 usersSeenDict[member.id] = True
         
         for userid in usersSeenDict:
             if not usersSeenDict[userid]:
                 self.bot.dbm.removeUser(userid, self.bot.user.id)
-                await self.bot.logToDebugUser(f"user maintenance: removed {userid}")
+                removed.append(userid)
         
-        await self.bot.logToDebugUser("user maintenance complete")
-
+        await self.bot.logToDebugUser(f'user maintenance complete, added:\n    {"\n    ".join(added)}\nRemoved:\n    {"\n    ".join(removed)}')
         
     @userMaintenance.before_loop
     async def before_printer(self):
