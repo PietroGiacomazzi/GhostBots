1c43e2a8160d499ffe2b97c2723ba1f8e82d0593
Pietro Giacomazzi <pietro.giacomazzi@gmail.com>
2022-12-12 14:05:52 +0100

    update default macros

--- database/upgrade_scripts/upgrade_006.sql
+++ database/upgrade_scripts/upgrade_006.sql
@@ -10,7 +10,8 @@ create table CharacterMacro(
 		on update cascade
 )Engine=InnoDB;
 
-insert into CharacterMacro(characterid, macroid, macrocommands) values (NULL, "wake", "me sangue -1\nsilent_mode_on\nme danni -1c\nsilent_mode_off\nme danni -1c");
+insert into CharacterMacro(characterid, macroid, macrocommands) values (NULL, "wake", "me sangue -1\n#me danni -1c\nme danni -1c");
+insert into CharacterMacro(characterid, macroid, macrocommands) values (NULL, "reset", "me forza reset\nme destrezza reset\nme costituzione reset\n\nme carisma reset\nme persuasione reset\nme aspetto reset\n\nme percezione reset\nme intelligenza reset\nme prontezza reset\n\nme salute reset");
 insert into CharacterMacro(characterid, macroid, macrocommands) values ("lilith", "wadjet", "me forza = 4\nme destrezza  = 3\nme costituzione = 2\nme percezione = 3\nme intelligenza = 3\nme prontezza = 3\nme carisma = 4\nme persuasione =  4\nme aspetto = 1"); 
 
 -- health management update
