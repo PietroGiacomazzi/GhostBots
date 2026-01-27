6b3f578fa3a04af46282fe407e28400d22635809
Pietro Giacomazzi <pietro.giacomazzi@gmail.com>
2026-01-27 23:16:04 +0100

    smorfia

--- bots/greedy_components/cogRoller.py
+++ bots/greedy_components/cogRoller.py
@@ -95,6 +95,122 @@ strat_list = [
                 'START'
                 ]
 
+smorfia_list = [
+    'jamme',
+    'fratm',
+    'maradona',
+    'juvemerda',
+    'napoli',
+    'vesuvio',
+    'osole',
+    'omare',
+    'kitammuort',
+    'chitemmuort',
+    'kitestramuort'
+]
+# these will be processed with endings
+smorfia_list_bonus = [
+    'start',
+    'strat'
+]
+
+smorfia_list = list(set(smorfia_list + [f(x) for x in (strat_list+smorfia_list_bonus) if x.endswith("t") for f in (lambda y: y+"m", lambda y: y+"amm", lambda y: y+"ammo", lambda y:y+"imm", lambda y: y+"immo")]))
+smorfia_list = smorfia_list + list(map(lambda x: x.upper(), smorfia_list))
+
+## this is a dict just for quick visual reference
+smorfia_napoli = {
+    1: "L'Italia",
+    2: "'A criatura",
+    3: "'A jatta",
+    4: "'O puorco",
+    5: "'A mano",
+    6: "Chella ca guarda 'nderra",
+    7: "'O vasetto",
+    8: "'A Madonna",
+    9: "'A figliata",
+    10: "'E fasule",
+    11: "'E surice",
+    12: "'E surdate",
+    13: "Sant'Antuono",
+    14: "'O 'mbriaco",
+    15: "'O guaglione",
+    16: "'O culo",
+    17: "'A disgrazzia",
+    18: "'O sanghe",
+    19: "'A resata",
+    20: "'A festa",
+    21: "'A femmena annuda",
+    22: "'O pazzo",
+    23: "'O scemo",
+    24: "'E Gguardie",
+    25: "Natale",
+    26: "Nanninella",
+    27: "'O càntaro",
+    28: "'E zizze",
+    29: "'O pate d''e ccriature",
+    30: "'E palle d''o tenente",
+    31: "'O patrone 'e casa",
+    32: "'O capitone",
+    33: "L'anne 'e Cristo",
+    34: "'A capa",
+    35: "L'auciello",
+    36: "'E castagnelle",
+    37: "'O monaco",
+    38: "'E mazzate",
+    39: "'A funa 'nganna",
+    40: "'A paposcia",
+    41: "'O curtiello",
+    42: "'O ccafè",
+    43: "Onna ô balcone",
+    44: "'E cancelle",
+    45: "'O vino bbuono",
+    46: "'E denare",
+    47: "'O muorto",
+    48: "'O muorto che pparla",
+    49: "'O piezzo 'e carne",
+    50: "'O ppane",
+    51: "'O ciardino",
+    52: "'A mamma",
+    53: "'O viecchio",
+    54: "'O cappiello",
+    55: "'A musica",
+    56: "'A caduta",
+    57: "'O scartellato",
+    58: "'O paccotto",
+    59: "'E pile",
+    60: "'O lamiento",
+    61: "'O cacciatore",
+    62: "'O muorto acciso",
+    63: "'A sposa",
+    64: "'A sciammerìa",
+    65: "'O chianto",
+    66: "'E ddoje zetelle",
+    67: "'O purpo into'â chitarra",
+    68: "'A zuppa cotta",
+    69: "sott'e 'ncoppa",
+    70: "'O palazzo",
+    71: "L'ommo 'e mmerda",
+    72: "'A maraviglia",
+    73: "'O spitale",
+    74: "'A rotta",
+    75: "Pulecenella",
+    76: "'A funtana",
+    77: "'E diavulille",
+    78: "'A bella figliola",
+    79: "'O mariuolo",
+    80: "'A vocca",
+    81: "E sciure",
+    82: "'A tavula 'mbandita",
+    83: "'O maletiempo",
+    84: "'A chiesa",
+    85: "l'aneme d''o priatorio",
+    86: "'A puteca",
+    87: "'E perucchie",
+    88: "'E casecavalle",
+    89: "'A vecchia",
+    90: "'A paura"
+}
+
 class GreedyGhostCog_Roller(gb.GreedyGhostCog):
     
     @commands.command(name = 'search', brief = "Cerca un tratto", description = "Cerca un tratto:\n\n .search <termine di ricerca> -> elenco dei risultati")
@@ -137,3 +253,12 @@ class GreedyGhostCog_Roller(gb.GreedyGhostCog):
     @commands.before_invoke(gs.command_security(gs.basicRegisteredUser))
     async def strat(self, ctx: commands.Context):
         await self.bot.atSend(ctx, f'{random.randint(1, 100)}, però la prossima volta scrivilo giusto <3')
+
+    @commands.command(name = 'smorfia', aliases = smorfia_list, brief = "Tira 1d100 per l'inizio giocata", description = "Tira 1d100 per l'inizio giocata, modalità Napoli")
+    @commands.before_invoke(gs.command_security(gs.basicRegisteredUser))
+    async def smorfia(self, ctx: commands.Context):
+        n = random.randint(1, 100)
+        command = ctx.message.content.split(" ")[0][1:]
+        uppercase_response = sum(map(lambda x: x.isupper(), command)) / len(command) > 0.5
+        response = f'{n}: {smorfia_napoli[n] if n<len(smorfia_napoli) else "fortunellə"}'
+        await self.bot.atSend(ctx, response.upper() if uppercase_response else response)
